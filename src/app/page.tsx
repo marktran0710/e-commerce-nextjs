eacf93f6d1c2a25464c8e2c11c0faa317b2f0e8c
hautran <tranleconghau772001@gmail.com>
2024-07-30 22:09:14 +0700

    add prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
-import MaxWidthWrapper from "@/components/MaxWitdhWrapper";
+import MaxWidthWrapper from '@/components/MaxWitdhWrapper';
 
 export default function Home() {
   return (
     <div className="bg-slate-50">
       <section>
-        <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3
-        sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52"><div></div></MaxWidthWrapper>
+        <MaxWidthWrapper
+          className="pb-24 pt-10 lg:grid lg:grid-cols-3
+        sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52"
+        >
+          <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
+            <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
+              <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                <img src="/snake-1.png" className="w-full" />
+              </div>
+              <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
+                Your Image on a <span>Custom</span> Phone Case
+              </h1>
+            </div>
+          </div>
+        </MaxWidthWrapper>
       </section>
     </div>
   );
